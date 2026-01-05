821e8060a72aabc01ffb55d8d0adbb86f43d27e6
Marcus Salviano <marcussalviano@gmail.com>
2026-01-04 23:25:00 -0400

    Remove a validação personalizada do CPF.
    Remove a anotação e o validador de validação CPF personalizados.
    
    Substitui a validação personalizada pelo validador do Hibernate.
    
    Isso simplifica o processo de validação e utiliza uma biblioteca bem testada.

--- src/main/java/com/fiap/snackapp/core/domain/vo/CPF.java
+++ src/main/java/com/fiap/snackapp/core/domain/vo/CPF.java
@@ -1,7 +1,11 @@
 package com.fiap.snackapp.core.domain.vo;
 
+
+import org.hibernate.validator.internal.constraintvalidators.hv.br.CPFValidator;
+
 public record CPF(String value) {
     public CPF {
+        CPFValidator cpfValidator = new CPFValidator();
         if (value == null || !value.matches("\\d{11}")) {
             throw new IllegalArgumentException("CPF inválido");
         }
